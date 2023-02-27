ae9e3ce8a0e9f27cdd3264978a8412f3eaef79e4
Luis Rodríguez <lfrz.arc@gmail.com>
2023-02-26 21:48:21 -0600

    Learning to validate forms and useRef

--- Data fetching/Search-movies/src/App.jsx
+++ Data fetching/Search-movies/src/App.jsx
@@ -1,9 +1,12 @@
 import { useMovies } from './Hooks/useMovies';
 import { RenderResults } from './Modules/Results';
 import { Search } from './Modules/Search';
+import { useRef } from 'react';
 
 export default function App () {
   const { movies: mappedMovies } = useMovies();
+  // useRef is ment to save data as useState, but when the value is updated the component don't gets re-rednered as in useState.
+  
 
   return (
     <div className='container'>
