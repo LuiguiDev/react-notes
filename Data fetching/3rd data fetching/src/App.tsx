290a7eb7acb2ed831fdd806c71a8c8c3ff45f39c
Luis Rodríguez <lfrz.arc@gmail.com>
2023-07-01 23:30:15 -0600

    fetch dominated

--- Data fetching/3rd data fetching/src/App.tsx
+++ Data fetching/3rd data fetching/src/App.tsx
@@ -1,44 +1,64 @@
-import { useState } from 'react'
+import { useDebugValue, useEffect, useState } from 'react'
 import './App.css'
 
-async function getFactAsync () {
-  const api = 'https://catfact.ninja/fact'
+const factApi = 'https://catfact.ninja/fact'
 
+// both getFact working, just different ways to get the same result
+async function getFactAsync () {
   try {
     const response = await fetch(api)
 
     if (!response.ok) {
       throw new Error(`Error HTTP: ${response.status}`)
     }
-
     const json = await response.json()
 
-    console.log(json)
+    return json
   } catch (error) {
     throw new Error('Failed to search birds')
   }
 }
 
-function getFact () {
-  const api = 'https://catfact.ninja/fact'
-
-  fetch(api)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`)
-      }
-
-      return response.json()
-    })
-    .then((data) => console.log(data))
-    .catch(error => console.log(error))
-}
-
-getFactAsync() // both getFact working, just different ways to get the same result
-
 function App () {
+  const [fact, setFact] = useState('')
+  const [imageULR, setImageURL] = useState('')
+
+  function getFact () {
+    fetch(factApi)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+      })
+      .catch(error => console.log(error))
+  }
+  function getImage (word: string) {
+    fetch(`https://cataas.com/cat/says/${word}?size=50&color=red&json=true`)
+      .then(data => data.json())
+      .then(json => {
+        const src = json.url
+        const url = `https://cataas.com${src}`
+        setImageURL(url)
+      })
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    getFact()
+    getImage('hi')
+  }, [])
+
   return (
-    <h1>Birds</h1>
+    <main>
+      <h1>Cat facts</h1>
+      {fact && <p>{fact}</p>}
+      {imageULR && <img src={imageULR} alt='Kitten saying hi' />}
+    </main>
   )
 }
 
