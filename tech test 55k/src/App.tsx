e41e9c56cba04682596d0724e3ed5d6d49a134ad
Luis Rodríguez <lfrz.arc@gmail.com>
2023-08-07 18:24:18 -0600

    Filter by country added

--- tech test 55k/src/App.tsx
+++ tech test 55k/src/App.tsx
@@ -5,14 +5,22 @@ import { UsersList } from './components/UsersList'
 
 const App = () => {
   const [users, setUsers] = useState<Users>([])
+  const [showColors, setShowColors] = useState<boolean>(false)
+  const [sortUsers, setSortUsers] = useState<boolean>(false)
 
-  function getUsers () {
+  function getUsers() {
     const maxResults = 100
 
     fetch(`https://randomuser.me/api/?results=${maxResults}`)
       .then(res => res.json())
       .then(data => setUsers(data.results))
       .catch(error => console.log(error))
+  }
+  function toggleshowColors() {
+    setShowColors(!showColors)
+  }
+  function toggleSortUsers() {
+    setSortUsers(prevState => !prevState)
   }
 
   useEffect(() => {
@@ -21,7 +29,17 @@ const App = () => {
 
   return (
     <>
-      <UsersList  users={users} />
+      <header>
+        <button onClick={toggleshowColors}>Show colors</button>
+        <button onClick={toggleSortUsers}>Sort by country</button>
+      </header>
+      <main>
+        <UsersList
+          users={users}
+          showColors={showColors}
+          sortUsers={sortUsers}
+        />
+      </main>
     </>
   )
 }
--- tech test 55k/src/components/UsersList.tsx
+++ tech test 55k/src/components/UsersList.tsx
@@ -1,21 +1,48 @@
 import { Root, User } from "../types"
 
 interface Props {
-  users: User[]
+  users: User[],
+  showColors: boolean,
+  sortUsers: boolean
 }
 
-export const UsersList = ({ users }: Props) => {
+export const UsersList = ({ users, showColors, sortUsers }: Props) => {
+
+  const sortedUsers = [...users].sort((a, b) => {
+    return a.location.country.localeCompare(b.location.country)
+  })
+  const showedUsers = sortUsers ? sortedUsers : users
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Photo</th>
+          <th>Picture</th>
           <th>Name</th>
           <th>Last Name</th>
           <th>County</th>
           <th>Actions</th>
         </tr>
       </thead>
+
+      <tbody>
+        {
+          showedUsers.map((user, index) => {
+            const isOdd = index % 2 === 0
+            const color = isOdd ? '#333' : '#555'
+            const isActive = showColors ? color : 'transparent'  
+            return (
+              <tr style={{backgroundColor: isActive}}>
+                <td> <img src={user.picture.thumbnail} alt="Profile picture" /></td>
+                <td>{user.name.first}</td>
+                <td>{user.name.last}</td>
+                <td>{user.location.country}</td>
+                <td><button>Delete</button></td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
     </table>
   )
 }
